a9a01dadf608b09854b05f6906c57705a91c3b4b
Yousaf Awais <yousafawais987@gmail.com>
2025-11-13 14:11:18 +0500

    "done"

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://bz-cart-d-ashboard.vercel.app",
+      "https://dashboardbzcart.vercel.app",
       "https://dashboardbzcart.vercel.app/",
       "https://bz-cart.vercel.app",
       "https://www.bzcart.store",
